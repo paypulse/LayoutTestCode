163784ccabf0a2c438f5013e7ad13ca772807726
paypulse <sourcedev5@gmail.com>
2017-11-02 20:05:10 +0900

    Add Blen xaml code

--- LoadXmlResources/LoadXmlResources/LoadXamlResources.cs
+++ LoadXmlResources/LoadXmlResources/LoadXamlResources.cs
@@ -6,36 +6,30 @@ using System.Windows.Markup;
 
 namespace LoadXmlResources
 {
+
     class LoadXamlResources :Window
     {
         [STAThread]
         public static void Main()
         {
             Application app = new Application();
-            app.Run(new LoadXamlResources());
-        }
-
-        public LoadXamlResources()
-        {
-            Title = "Load Xaml Resouces";
+          
             Uri uri = new Uri("pack://application:,,,/LoadXamlResources.xml");
             Stream stream = Application.GetResourceStream(uri).Stream;
-            FrameworkElement el = XamlReader.Load(stream) as FrameworkElement;
-            Content = el;
+            Window win = XamlReader.Load(stream) as Window;
+            win.AddHandler(Button.ClickEvent, new RoutedEventHandler(ButtonOnClick));
 
-            Button btn = el.FindName("My Button") as Button;
-            if(btn != null)
-            {
-                btn.Click += ButtonOnClick;
-            }
-
+            app.Run(win);
         }
 
-        void ButtonOnClick(object sender, RoutedEventArgs args)
+        static void ButtonOnClick(object sender, RoutedEventArgs args)
         {
             MessageBox.Show("The button labeled'"+ (args.Source as Button).Content +
                 "' has been clicked");
         }
+
+        /*각종 handler를 추가 하자. */
+
 
 
     }
